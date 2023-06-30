0f74e777442dc6f7d1500c8e1943cd84014aa237
majo2210 <mjescalonar2@gmail.com>
2023-06-30 13:13:55 -0500

    cambios js

--- js/presentacion.js
+++ js/presentacion.js
@@ -1,13 +1,11 @@
-classList.add('active')
-
-var menuItems = document.querySelectorAll('#nav-header');
+var menuItems = document.querySelectorAll('#nav-header li');
 
 // Recorre todos los ítems y agrega un evento de clic a cada uno
 menuItems.forEach(function(nav) {
-  item.addEventListener('click', function() {
+  nav.addEventListener('click', function() {
     // Remueve la clase "active" de todos los ítems
-    menuItems.forEach(function(nav) {
-      item.classList.remove('active');
+    menuItems.forEach(function(navItem) {
+      navItem.classList.remove('active');
     });
 
     // Agrega la clase "active" al ítem clicado
@@ -16,13 +14,8 @@ menuItems.forEach(function(nav) {
 });
 
 // Aplica la clase "active" inicialmente solo a los ítems "Inicio" y "Contacto"
-var inicioItem = document.querySelector('nav li:first-child');
-var contactoItem = document.querySelector('nav li:last-child');
+var inicioItem = document.querySelector('#nav-header li:first-child');
+var contactoItem = document.querySelector('#nav-header li:last-child');
 
 inicioItem.classList.add('active');
 contactoItem.classList.add('active');
-
-
-
-
-
